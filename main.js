6890851ff7bae72789b34910a8b91d385b72f8da
Brian McKeever <keever@gmail.com>
2016-07-19 11:24:13 -0600

    proof of concept

--- main.js
+++ main.js
@@ -52,4 +52,47 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
+function test(path){
+    console.log(path);
+    var homePath = app.getPath("desktop");
+    var fs = require('fs');
+    var parserOutputLocation = homePath + "/fg_parser_output.zip";
+    if(fs.stat(parserOutputLocation, function(error, stats){
+        if(error){
+            if(error.code != "ENOENT"){
+                console.log(error);
+                return;
+            } // else output file exists
+        } else if(stats.isFile()){
+            fs.unlinkSync(parserOutputLocation);
+        } else {
+            console.log("parserOutputLocation is weird");
+        }
+    }));
 
+    var JSZip = require("jszip");
+    var zip = new JSZip();
+    zip.file("Hello.txt", "Hello World\n");
+    var img = zip.folder("images");
+    zip
+    .generateNodeStream({type:'nodebuffer',streamFiles:true})
+    .pipe(fs.createWriteStream(parserOutputLocation))
+    .on('finish', function () {
+        // JSZip generates a readable stream with a "end" event,
+        // but is piped here in a writable stream which emits a "finish" event.
+        console.log("fg_parser_output.zip written.");
+    });
+
+    global.fg_parser = true
+}
+
+const {ipcMain} = require('electron');
+ipcMain.on('input-path', (event, arg) => {
+  console.log(arg);  // prints "ping"
+  event.sender.send('asynchronous-reply', 'pong');
+});
+
+ipcMain.on('synchronous-message', (event, arg) => {
+  console.log(arg);  // prints "ping"
+  event.returnValue = 'pong';
+});
